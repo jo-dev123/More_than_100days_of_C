7d4b292e04ee9fe237b00c673ac19615993684b2
jo-dev123 <johnsondegbor@gmail.com>
2022-05-23 08:03:07 -0700

    adding File Copying

---
+++ ASCI_C/Introduction/File_Copying/Ex1-7.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * @void no parameter
+ *
+ * Description: Write a program to print the value of EOF
+ */
+
+int main(void)
+{
+	printf("EOF = %d\n", EOF);
+	return (0);	//SUCCESS. Exit execution
+}
