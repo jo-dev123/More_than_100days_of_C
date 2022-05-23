7cf7c3f977dd37af945f3ab0dac216b861433e14
jo-dev123 <johnsondegbor@gmail.com>
2022-05-23 04:04:52 -0700

    correcting Fahrenheit formula

--- ASCI_C/Introduction/Variables_and_Arithmetic_Expressions/Ex1-4.c
+++ ASCI_C/Introduction/Variables_and_Arithmetic_Expressions/Ex1-4.c
@@ -22,7 +22,7 @@ main(void)
 	
 	for (Cel = 0; Cel <= 300; Cel += 20) 	//for statement (read 1.3) and 'Cel += 20' = 'Cel = Cel + 20
 	{
-		Fah = ((5.0 * Cel) / 9.0) + 32;		//Convert to Celsius and save to Cel
+		Fah = ((9.0 * Cel) / 5.0) + 32;		//Convert to Celsius and save to Cel
 		printf("%3.1f %6.1f\n", Cel, Fah);	// Print Result
 	}
 
